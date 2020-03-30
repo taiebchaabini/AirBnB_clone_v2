a25f3507d4c278810c1cd3950da8e0ad5de615c4
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-03-31 00:08:05 +0100

    try the content of branch

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,18 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls is None:
+            return self.__objects
+        d = {}
+        for k, v in self.__objects.items():
+            if v.__class__ == cls:
+                d[k] = v
+        return d
 
     def new(self, obj):
         """sets __object to given obj
@@ -53,5 +59,14 @@ class FileStorage:
                 for key, value in (json.load(f)).items():
                     value = eval(value["__class__"])(**value)
                     self.__objects[key] = value
+                    self.new(value)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ deletes an object from the dict __objects if it's inside
+        """
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            del self.__objects[key]
+            self.save()
