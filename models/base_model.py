3af51bf54934bf990b6a61d4301269d87b3bdf8c
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-03-30 12:54:31 +0100

    update the create function to accept many parameters

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,8 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -27,8 +29,6 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
@@ -48,6 +48,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
