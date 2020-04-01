5516ba5ec1b0f4ffc5ec79c2d8ce5c49cf72590f
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-04-01 17:19:55 +0100

    map classes to tables, link tables and fix all possible errors

--- models/place.py
+++ models/place.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """This is the place class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
+from models.review import Review
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """This is the class for Place
     Attributes:
         city_id: city id
@@ -18,14 +21,31 @@ class Place(BaseModel):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", cascade="all,delete", backref="place")
     amenity_ids = []
+
+    @property
+    def reviews(self):
+        """
+        the getter attribute reviews returns
+        the list of Review instances
+        with place_id equals to the current Place.id
+        """
+        from models import storage
+        r = {}
+        all_reviews = storage.all(Review)
+        for k, v in all_reviews.items():
+            if v.place_id == self.id:
+                r[k] = v
+        return r
