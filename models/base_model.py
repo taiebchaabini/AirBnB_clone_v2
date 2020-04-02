66431b982fce65dbee84a008bfe4af981bed3c4c
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-04-02 22:43:05 +0100

    change base_model to handle object creation with kwaargs that don't contain necessary attributes

--- models/base_model.py
+++ models/base_model.py
@@ -29,9 +29,9 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
         if kwargs:
-            if "id" not in kwargs.items():
-                self.id = str(uuid.uuid4())
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
