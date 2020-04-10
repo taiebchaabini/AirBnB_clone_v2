7672ba6fa2885ba34d23d085e7926efb84bb21f1
taiebchaabini <taiebchaabini@gmail.com>
2020-04-10 14:25:44 +0000

    Adding regexp and location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,4 +7,10 @@ mkdir -p /data/web_static/shared/
 echo "This is my sample content" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current 
 chown ubuntu:ubuntu -R /data/
+regex='location \/ \{\(.|\s\)*\}'
+location="\n\n\tlocation \/hbtn_static\/ \{\
+\n\t\talias \/data\/web_static\/current\/\
+\n\t\tautoindex off\;\
+\n\t\}"
+sed -i -r "s/$regex/\0$location/g" /etc/nginx/sites-enabled/default
 service nginx restart
