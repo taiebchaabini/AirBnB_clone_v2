994b764bf0c0449023d6b48d68ee58c677da8daf
taiebchaabini <taiebchaabini@gmail.com>
2020-04-10 17:06:14 +0000

    Adding service restart

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,6 +2,7 @@
 # Bash script that sets up your web servers for the deployment of web_static.
 apt-get update -y
 apt-get install -y nginx
+service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/ 
 echo "This is my sample content" > /data/web_static/releases/test/index.html
@@ -13,5 +14,4 @@ location="\n\n\tlocation \/hbtn_static\/ \{\
 \n\t\tautoindex off\;\
 \n\t\}"
 sed -i -r "s/$regex/$location\n\n\0/g" /etc/nginx/sites-enabled/default
-service nginx stop
-service nginx start
+service nginx restart
