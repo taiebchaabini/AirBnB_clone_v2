5c5b71fa9b96ed9f0079e39b52bb5f6595f4c66f
taiebchaabini <taiebchaabini@gmail.com>
2020-04-21 20:11:10 +0000

    New file

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+Script that starts a Flask web application
+"""
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+app.run(host = '0.0.0.0', port = 5000)
