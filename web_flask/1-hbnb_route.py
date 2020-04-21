ec20dc192a80fdf4703211d843f7f71d155d04d0
taiebchaabini <taiebchaabini@gmail.com>
2020-04-21 20:34:31 +0000

    New file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+Script that starts a Flask web application
+"""
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """
+    Return a simple string for index page
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Return a simple string for hbnb page
+    """
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
