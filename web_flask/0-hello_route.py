c48638dcf8bcbb80a0aaaf31860ed187f3f1135e
taiebchaabini <taiebchaabini@gmail.com>
2020-04-21 20:15:02 +0000

    PEP8 fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ Script that starts a Flask web application
 """
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
 @app.route('/')
 def hello_hbnb():
-    """ 
+    """
     Return a simple string for index page
     """
     return 'Hello HBNB!'
-app.run(host = '0.0.0.0', port = 5000)
+app.run(host='0.0.0.0', port=5000)
